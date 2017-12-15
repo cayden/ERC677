0958446339efd56b0b8cd4b4c25653d4912eacfd
Sven Stucki <git@svenstucki.ch>
2017-12-15 17:20:40 +0100

    Fix warnings

--- contracts/ERC677.sol
+++ contracts/ERC677.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.18;
+
 /**
  * @title ERC677 transferAndCall token interface
  * @dev See https://github.com/ethereum/EIPs/issues/677 for specification and
@@ -6,5 +8,5 @@
 contract ERC677 {
     event Transfer(address indexed _from, address indexed _to, uint256 _amount, bytes _data);
 
-    function transferAndCall(address _receiver, uint _amount, bytes _data);
+    function transferAndCall(address _receiver, uint _amount, bytes _data) public;
 }
--- contracts/ERC677Receiver.sol
+++ contracts/ERC677Receiver.sol
@@ -1,8 +1,10 @@
+pragma solidity ^0.4.18;
+
 /**
  * @title Receiver interface for ERC677 transferAndCall
  * @dev See https://github.com/ethereum/EIPs/issues/677 for specification and
  *      discussion.
  */
 contract ERC677Receiver {
-    function tokenFallback(address _from, uint _amount, bytes _data);
+    function tokenFallback(address _from, uint _amount, bytes _data) public;
 }
